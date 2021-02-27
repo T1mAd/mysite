892187abcfd9a8d36c4da9e7991eb6d6ca754f25
T1mAd <adam.tima.tomsk@gmail.com>
2021-02-27 19:53:29 +0700

    27.02.21

--- polls/views.py
+++ polls/views.py
@@ -1,10 +1,36 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Question, Choice
+
+
+
+
 
 # Create your views here.
 def index(request):
-    return HttpResponse('<h1>Hello world</h1>')
+    q_all = Question.objects.all()
+    res = "<ol>"
+    for q in q_all:
+        res += "<li>%s</li>"% q.text
+    res += "</ol>"
+    return HttpResponse(res)
 
 def meme(request):
     return HttpResponse('<img src = "https://memepedia.ru/wp-content/uploads/2018/09/bongo-cat.jpg">')
+
+def detail(request, q_id):
+    res = "Question number %s."% q_id,
+    return HttpResponse(res)
+
+def results(request, q_id):
+    res = "Result for question number %s."% q_id,
+    return HttpResponse(res)
+
+def vote(request, q_id):
+    res = "Vote for question number %s."% q_id,
+    return HttpResponse(res)
+
+
+
+
