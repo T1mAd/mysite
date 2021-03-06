9c73eb690e3646a940b292d498a388db329a71b1
T1mAd <adam.tima.tomsk@gmail.com>
2021-03-06 19:56:42 +0700

    06.03.21

--- polls/views.py
+++ polls/views.py
@@ -9,19 +9,35 @@ from .models import Question, Choice
 
 # Create your views here.
 def index(request):
-    q_all = Question.objects.all()
-    res = "<ol>"
-    for q in q_all:
-        res += "<li>%s</li>"% q.text
-    res += "</ol>"
-    return HttpResponse(res)
+    questions = Question.objects.all()
+
+    context = {
+        "questions" : questions
+    }
+
+
+
+
+
+
+    return render(request, "polls/index.html", context)
 
 def meme(request):
     return HttpResponse('<img src = "https://memepedia.ru/wp-content/uploads/2018/09/bongo-cat.jpg">')
 
 def detail(request, q_id):
-    res = "Question number %s."% q_id,
-    return HttpResponse(res)
+    question = Question.objects.get(pk=q_id)
+
+    context = {
+        "question" : question
+    }
+
+    return render(request, "polls/detail.html", context)
+
+
+
+
+
 
 def results(request, q_id):
     res = "Result for question number %s."% q_id,
